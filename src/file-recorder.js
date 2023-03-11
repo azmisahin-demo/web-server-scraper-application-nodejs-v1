2681d688d4613695f679f601da4c09df1ff59bda
Azmi ŞAHİN <azmisahin@users.noreply.github.com>
2023-03-11 18:43:44 +0000

    ✨: meet file recorder

--- src/file-recorder.js
+++ src/file-recorder.js
@@ -6,6 +6,12 @@
  */
 
 /**
+ * dependencies
+ */
+const fs = require('fs')
+const path = require('path')
+
+/**
  * @class
  * FileRecorder Library
  *
@@ -39,6 +45,47 @@ class FileRecorder {
   constructor() {
     // return instance
     return this
+  }
+
+  /**
+   * Is there a directory or file?
+   * @param {string} fullPath
+   * @returns bool
+   */
+  async _isThere(fullPath) {
+    // Is there a directory or file?
+    return await fs.existsSync(fullPath)
+  }
+
+  /**
+   * Configure folder path
+   * @param {string} folderPath
+   * @returns bool
+   */
+  async _configureFolder(folderPath) {
+    // configure folder
+    return (await this._isThere(folderPath)) || fs.mkdirSync(folderPath, { recursive: true })
+  }
+
+  /**
+   * Write a file
+   * @param {string} folderPath folder path
+   * @param {string} fileName file name
+   * @param {string} data data
+   * @returns bool
+   */
+  async sync(folderPath, fileName, data) {
+    // os file path
+    const filePath = path.join(folderPath, fileName)
+
+    // configure folder
+    await this._configureFolder(folderPath)
+
+    // file writing
+    await fs.writeFileSync(filePath, data)
+
+    // created
+    return await this._isThere(filePath)
   }
 }
 
